3f85dee5a906346622099ba533fcf4daeabd6646
lando101 <landon.messmer2019@gmail.com>
2023-12-28 03:04:20 -0500

    polish

--- src/app/chat/components/message-list/message-list.component.ts
+++ src/app/chat/components/message-list/message-list.component.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { cssInfo } from '@ng-icons/css.gg';
+import { cssInfo, cssBot, cssUser } from '@ng-icons/css.gg';
 
 @Component({
   selector: 'app-message-list',
   standalone: true,
   imports: [CommonModule, ScrollPanelModule, NgIconComponent],
-  viewProviders: [provideIcons({ cssInfo })],
+  viewProviders: [provideIcons({ cssInfo, cssBot, cssUser })],
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.sass'
 })

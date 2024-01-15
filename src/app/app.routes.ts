34e46663afc80002ef1ef8bfe9ad1c3369ad50df
lando101 <landon.messmer2019@gmail.com>
2024-01-15 00:18:24 -0500

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,17 @@ import { AuthGuard } from './core/auth.guard';
 import { AboutComponent } from './pages/about/about.component';
 import { AuthComponent } from './pages/auth/auth.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { AssistantComponent } from './pages/assistant/assistant.component';
+import { ThreadWindowComponent } from './chat/components/thread-window/thread-window.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'assistant', component: AssistantComponent, canActivate: [AuthGuard], children: [
+    {path: ':threadId', component: ThreadWindowComponent },
+    {path: '', component: ThreadWindowComponent }
+  ] },
   { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
   { path: 'auth', component: AuthComponent },
   { path: 'sign-up', component: SignupComponent }
---
+++ src/app/pages/assistant/assistant.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-assistant',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './assistant.component.html',
+  styleUrl: './assistant.component.sass'
+})
+export class AssistantComponent {
+
+}
